831afcb92a90aa816f66526bba1167d62858503d
Emanuel Orozco <Emanuelalex3500@gmail.com>
2024-11-17 23:25:59 -0600

    Alcaldes y fondo

--- client/src/home/AlcaldesList.jsx
+++ client/src/home/AlcaldesList.jsx
@@ -1,57 +1,75 @@
+import a1 from '/assets_muni/1.jpeg'
+import a2 from '/assets_muni/2.jpeg'
+import a3 from '/assets_muni/3.jpeg'
+import a4 from '/assets_muni/4.jpeg'
+import a5 from '/assets_muni/5.jpeg'
+import a6 from '/assets_muni/6.jpeg'
+import a7 from '/assets_muni/7.jpeg'
+import a8 from '/assets_muni/8.jpeg'
+import a9 from '/assets_muni/9.jpeg'
+import a10 from '/assets_muni/10.jpeg'
+import a11 from '/assets_muni/11.jpeg'
+import a12 from '/assets_muni/12.jpeg'
+import a13 from '/assets_muni/13.jpeg'
+import a14 from '/assets_muni/14.jpeg'
+import a15 from '/assets_muni/15.jpeg'
+import a16 from '/assets_muni/16.jpeg'
+import a17 from '/assets_muni/17.jpeg'
+import a18 from '/assets_muni/18.jpeg'
+import a19 from '/assets_muni/19.jpeg'
+import actual from '/assets_muni/actual.jpeg'
+import emptyImage from '/assets_muni/empty.jpg'
+
 export function AlcaldesList() {
     const alcaldeActual = {
         nombre: "Armando Ixtabalan Gómez",
-        periodo: "2024-2028"
+        periodo: "2024-2028",
+        image: actual,
     }
 
     const alcaldesAnteriores = [
-        { nombre: "Abrahim Zelada López", periodo: "2021-2024" },
-        { nombre: "Abrahim Zelada López", periodo: "2016-2020" },
-        { nombre: "Juan Cástulo López Xicará", periodo: "2012-2016" },
-        { nombre: "Augusto Rene Escobar de León", periodo: "2008-2012" },
-        { nombre: "Augusto Rene Escobar de León", periodo: "2004-2008" },
-        { nombre: "Augusto Rene Escobar de León", periodo: "2000-2004" },
-        { nombre: "Pablo Rolando Soch López", periodo: "1996-2,000" },
-        { nombre: "Pablo Rolando Soch López", periodo: "1993-1996" },
-        { nombre: "Francisco Santos Gonón" },
-        { nombre: "Eleuterio de Jesús Escobar de Paz" },
-        { nombre: "Pablo Rolando Soch López", periodo: "1986-1988" },
-        { nombre: "David Antonio Hurtado Mazariegos" },
-        { nombre: "Cirilo Ortiz Jocol" },
-        { nombre: "Pablo de Jesús Escobar Mazariegos" },
-        { nombre: "Carlos Armando Aguilar Maldonado" },
-        { nombre: "Agustín Santos Cifuentes" },
-        { nombre: "Diego Martin Miranda Alvarado" },
-        { nombre: "Manuel Beletzuy H." },
-        { nombre: "Cesar Augusto Soto Barrios" },
-        { nombre: "Ramón Guinac García" },
-        { nombre: "Rosario Escobar Barrios" },
-        { nombre: "Candido Gonon" },
-        { nombre: "Desiderio Coyoy" },
-        { nombre: "Cresencio Orox6m" },
-        { nombre: "Manuel Morales" },
-        { nombre: "Victor Rodas Reyes" },
-        { nombre: "Marcelino Jocol" },
-        { nombre: "Florencio Escobar" },
-        { nombre: "Transito Rodas" },
-        { nombre: "Ramiro Rodas Reyes" },
-        { nombre: "Erasmo Reyes" },
-        { nombre: "Jacinto D. Ordofiez" },
-        { nombre: "Tiburcio Santos" },
-        { nombre: "Eusebio Oroxém" },
-        { nombre: "Alberto Loarca" },
-        { nombre: "Basilio Santos" },
-        { nombre: "Carlos Escobar" },
-        { nombre: "Bonifacio Coyoy" },
-        { nombre: "Alejandro Aguilar Samayoa" },
-        { nombre: "Feliciano Minera" },
-        { nombre: "Carlos Cirilo Rosal" },
-        { nombre: "Macario Arango" },
-        { nombre: "Manuel Beletzuy" },
-        { nombre: "Cipriano Garcia" },
-        { nombre: "Basilio Garcia" },
-        { nombre: "Cresencio Cotom" },
-        { nombre: "Jacinto Escobar" },
+        {nombre: "Abrahim Zelada López", periodo: "2021-2024, 2016-2020", image: a1},
+        {nombre: "Juan Cástulo López Xicará", periodo: "2012-2016", image: a2},
+        {nombre: "Augusto Rene Escobar de León", periodo: "2008-2012, 2004-2008, 2000-2004", image: a3},
+        {nombre: "Pablo Rolando Soch López", periodo: "1996-2000, 1993-1996, 1986-1988", image: a4},
+        {nombre: "Francisco Santos Gonón", image: a5},
+        {nombre: "Eleuterio de Jesús Escobar de Paz", image: a6},
+        {nombre: "David Antonio Hurtado Mazariegos", image: a18},
+        {nombre: "Cirilo Ortiz Jocol"},
+        {nombre: "Pablo de Jesús Escobar Mazariegos", image: a16},
+        {nombre: "Carlos Armando Aguilar Maldonado", image: a11},
+        {nombre: "Agustín Santos Cifuentes", image: a10},
+        {nombre: "Diego Martin Miranda Alvarado", image: a17},
+        {nombre: "Manuel Beletzuy H."},
+        {nombre: "Cesar Augusto Soto Barrios"},
+        {nombre: "Ramón Guinac García", image: a14},
+        {nombre: "Rosario Escobar Barrios", image: a7},
+        {nombre: "Candido Gonon", image: a12},
+        {nombre: "Desiderio Coyoy", image: a8},
+        {nombre: "Cresencio Oroxóm"},
+        {nombre: "Manuel Morales"},
+        {nombre: "Victor Rodas Reyes", image: a9},
+        {nombre: "Marcelino Jocol", image: a13},
+        {nombre: "Florencio Escobar"},
+        {nombre: "Transito Rodas"},
+        {nombre: "Ramiro Rodas Reyes"},
+        {nombre: "Erasmo Reyes"},
+        {nombre: "Jacinto D. Ordofiez"},
+        {nombre: "Tiburcio Santos"},
+        {nombre: "Eusebio Oroxém"},
+        {nombre: "Alberto Loarca"},
+        {nombre: "Basilio Santos", image: a15},
+        {nombre: "Carlos Escobar"},
+        {nombre: "Bonifacio Coyoy"},
+        {nombre: "Alejandro Aguilar Samayoa"},
+        {nombre: "Feliciano Minera"},
+        {nombre: "Carlos Cirilo Rosal"},
+        {nombre: "Macario Arango"},
+        {nombre: "Manuel Beletzuy"},
+        {nombre: "Cipriano Garcia"},
+        {nombre: "Basilio Garcia"},
+        {nombre: "Cresencio Cotom"},
+        {nombre: "Jacinto Escobar", image: a19},
     ]
 
     return (
@@ -59,11 +77,15 @@ export function AlcaldesList() {
             <h3 className="mb-4">Alcaldes de La Esperanza</h3>
 
             {/* Alcalde Actual */}
-            <div className="card mb-4">
-                <div className="card-body">
-                    <h4 className="card-title">Alcalde Actual</h4>
-                    <h5 className="card-subtitle mb-2 text-muted">{alcaldeActual.nombre}</h5>
-                    <p className="card-text">Periodo: {alcaldeActual.periodo}</p>
+            <div className="row align-items-center justify-content-center">
+                <div className="card mb-4 col-md-4 col-12">
+                    <img src={alcaldeActual.image ? alcaldeActual.image : emptyImage}
+                         alt={`Alcalde ${alcaldeActual.nombre}`}/>
+                    <div className="card-body text-center">
+                        <h4 className="card-title">Alcalde Actual</h4>
+                        <h5 className="card-subtitle mb-2 text-muted">{alcaldeActual.nombre}</h5>
+                        <p className="card-text">Periodo: {alcaldeActual.periodo}</p>
+                    </div>
                 </div>
             </div>
 
@@ -73,10 +95,11 @@ export function AlcaldesList() {
                 {alcaldesAnteriores.map((alcalde, index) => (
                     <div key={index} className="col">
                         <div className="card h-100">
-                            <div className="card-body">
-                                <h5 className="card-title">{alcalde.nombre}</h5>
+                            <img src={alcalde.image ? alcalde.image : emptyImage} alt={`Alcalde ${alcalde.nombre}`}/>
+                            <div className="card-body align-items-center justify-content-center d-flex flex-column">
+                                <h5 className="card-title text-center">{alcalde.nombre}</h5>
                                 {alcalde.periodo && (
-                                    <p className="card-text">Periodo: {alcalde.periodo}</p>
+                                    <p className="card-text text-center">Periodos: {alcalde.periodo}</p>
                                 )}
                             </div>
                         </div>
--- client/src/home/index.jsx
+++ client/src/home/index.jsx
@@ -1,83 +1,93 @@
 import {PhoneDirectory} from "src/home/PhoneDirectory.jsx";
 import {OfficeDirectory} from "src/home/OfficeDirectory.jsx";
 import logo from '/logo.jpg';
+import consejoMunicipal from '/assets_muni/consejo.jpeg';
 
 export function HomePage() {
     return (
-        <div className="container py-5">
-            <header className="text-center mb-5">
-                <h1 className="display-4 mb-3">MUNICIPALIDAD DE LA ESPERANZA</h1>
-                <p className="lead text-muted">
-                    DEPARTAMENTO DE QUETZALTENANGO, GUATEMALA, C.A.
-                </p>
-            </header>
-
-            <section className="mb-5 d-flex align-items-center justify-content-center">
-                <img
-                    src={logo}
-                    alt="Logo de la municipalidad"
-                    width={300}
-                    height={300}
-                />
-            </section>
-
-            <div className="row mb-5">
-                <div className="col-md-6 mb-4 mb-md-0">
-                    <div className="card h-100">
-                        <div className="card-body">
-                            <h2 className="card-title h4">Misión</h2>
-                            <p className="card-text">
-                                Somos un gobierno municipal que tiene como
-                                fin primordial prestar y administrar los
-                                servicios básicos, promover e impulsar
-                                proyectos de desarrollo comunitario para los
-                                vecinos bajo la jurisdicción territorial,
-                                garantizando su funcionamiento y
-                                mantenimiento de una forma eficiente y
-                                segura, mejorando con ello la calidad de vida
-                                de los habitantes.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6">
-                    <div className="card h-100">
-                        <div className="card-body">
-                            <h2 className="card-title h4">Visión</h2>
-                            <p className="card-text">
-                                El gobierno municipal del Municipio de la
-                                Esperanza, generara condiciones y
-                                oportunidades para que los vecinos alcancen
-                                un alto nivel en la calidad de vida, en un
-                                municipio moderno y seguro, donde el
-                                desarrollo se promueva de manera integral y
-                                sustentable; aprovechando los recursos y
-                                potencialidades con que cuenta nuestro
-                                municipio.
-                            </p>
-                        </div>
-                    </div>
-                </div>
+        <>
+            <div className="img-muni">
             </div>
+            <div className="container py-5">
+            <header className="text-center mb-5">
+                    <h1 className="display-4 mb-3">MUNICIPALIDAD DE LA ESPERANZA</h1>
+                    <p className="lead text-muted">
+                        DEPARTAMENTO DE QUETZALTENANGO, GUATEMALA, C.A.
+                    </p>
+                </header>
 
-            <section className="mb-5">
-                <h2 className="h3 mb-4">Nuestros valores</h2>
-                <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
-                    {["Servicio", "Excelencia", "Puntualidad", "Responsabilidad", "Trabajo en equipo", "Comunicación", "Honestidad"].map((value) => (
-                        <div key={value} className="col">
-                            <div className="card">
-                                <div className="card-body">
-                                    <h3 className="card-title h5">{value}</h3>
-                                </div>
+                <section className="mb-5 d-flex align-items-center justify-content-center">
+                    <img
+                        src={logo}
+                        alt="Logo de la municipalidad"
+                        width={300}
+                        height={300}
+                    />
+                </section>
+
+                <div className="row mb-5">
+                    <div className="col-md-6 mb-4 mb-md-0">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h2 className="card-title h4">Misión</h2>
+                                <p className="card-text">
+                                    Somos un gobierno municipal que tiene como
+                                    fin primordial prestar y administrar los
+                                    servicios básicos, promover e impulsar
+                                    proyectos de desarrollo comunitario para los
+                                    vecinos bajo la jurisdicción territorial,
+                                    garantizando su funcionamiento y
+                                    mantenimiento de una forma eficiente y
+                                    segura, mejorando con ello la calidad de vida
+                                    de los habitantes.
+                                </p>
                             </div>
                         </div>
-                    ))}
+                    </div>
+                    <div className="col-md-6">
+                        <div className="card h-100">
+                            <div className="card-body">
+                                <h2 className="card-title h4">Visión</h2>
+                                <p className="card-text">
+                                    El gobierno municipal del Municipio de la
+                                    Esperanza, generara condiciones y
+                                    oportunidades para que los vecinos alcancen
+                                    un alto nivel en la calidad de vida, en un
+                                    municipio moderno y seguro, donde el
+                                    desarrollo se promueva de manera integral y
+                                    sustentable; aprovechando los recursos y
+                                    potencialidades con que cuenta nuestro
+                                    municipio.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </section>
-            <PhoneDirectory/>
 
-            <OfficeDirectory/>
+                <section className="mb-5">
+                    <h2 className="h3 mb-4">Nuestros valores</h2>
+                    <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-4">
+                        {["Servicio", "Excelencia", "Puntualidad", "Responsabilidad", "Trabajo en equipo", "Comunicación", "Honestidad"].map((value) => (
+                            <div key={value} className="col">
+                                <div className="card">
+                                    <div className="card-body">
+                                        <h3 className="card-title h5">{value}</h3>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+                <div className="row align-items-center justify-content-center mb-5">
+                    <div className="col-12 col-md-8 col-lg-6">
+                        <img className="img-fluid" src={consejoMunicipal} alt="Consejo municipal"/>
+                    </div>
+                </div>
+                <PhoneDirectory/>
 
-        </div>
+                <OfficeDirectory/>
+
+            </div>
+        </>
     )
 }
